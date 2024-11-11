63697de9c5a5734c04fb837266ced70772fb78cb
Aditya Bhojane <118344077+AdityaBhojane@users.noreply.github.com>
2024-11-11 06:25:59 +0000

    react+tailwind setup code

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='text-center mt-40'>
+      <h1 className='text-4xl my-5'>Hello ! your setup is installed</h1>
+      <h1 className='text-xl text-yellow-300'>ReactJs + tailwind</h1>
+    </div>
+    
+  )
+}
+
+export default App
